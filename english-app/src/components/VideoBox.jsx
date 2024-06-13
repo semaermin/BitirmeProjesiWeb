b17f425c2d81c983fae017d1f820cd64381eca7d
Serhat Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2024-06-13 12:17:47 +0300

    Video Page updated. Videos are coming to Video Page.

--- english-app/src/components/VideoBox.jsx
+++ english-app/src/components/VideoBox.jsx
@@ -1,34 +1,69 @@
 import '../assets/styles/components/video-box.scss';
 import { useTheme } from '../context/ThemeContext';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function VideoBox() {
   const { theme } = useTheme();
+  const [videoQuestions, setVideoQuestions] = useState([]);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  useEffect(() => {
+    fetchVideoQuestions();
+  }, []);
+
+  const fetchVideoQuestions = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/video-list');
+      setVideoQuestions(response.data.videoQuestions); // Videolu soruları güncelle
+    } catch (error) {
+      console.error('Error fetching video questions:', error);
+    }
+  };
+
+  const handleNextVideo = () => {
+    setCurrentVideoIndex(
+      (prevIndex) => (prevIndex + 1) % videoQuestions.length
+    );
+  };
+
+  const currentVideo = videoQuestions[currentVideoIndex];
+
+  console.log(currentVideo);
 
   return (
     <div className={theme}>
       <div className="video-container">
-        <div className="video-box">
-          <div className="video">Video</div>
-          <div className="video-texts-container">
-            <p className="video-text">
-              Lorem ipsum dolor sit am et consectetur, adipisicing elit.
-              Expedita magnam reiciendis minima voluptates dolore sint nemo at
-              illum fugiat ipsam nesciunt necessitatibus dicta modi deleniti.
-            </p>
-            <div className="video-choices">
-              <button>
-                Lorem ipsum dolor sit am et consectetur, adipisicing elit.
-                Expedita magnam reiciendis minima voluptates dolore sint nemo at
-                illum
-              </button>
-              <button>
-                Lorem ipsum dolor sit am et consectetur, adipisicing elit.
-                Expedita magnam reiciendis minima voluptates dolore sint nemo at
-                illum
-              </button>
+        {currentVideo && (
+          <div className="video-box">
+            <div className="video">
+              <video autoPlay controls key={currentVideo.id}>
+                <source
+                  src={`http://localhost:8000/storage/${currentVideo.media_path}`}
+                  type="video/mp4"
+                />
+              </video>
+            </div>
+            <div className="video-texts-container">
+              <p className="video-text">
+                {currentVideo.text}
+                <button
+                  onClick={handleNextVideo}
+                  disabled={videoQuestions.length === 0}
+                >
+                  Sıradaki Video
+                </button>
+              </p>
+              <div className="video-choices">
+                {currentVideo.answers?.map((answer, index) => (
+                  <button value={answer.id} id={answer.id} key={index}>
+                    {answer.text}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- english-app/src/pages/VideoPage.jsx
+++ english-app/src/pages/VideoPage.jsx
@@ -1,61 +1,11 @@
 import Navbar from '../components/Navbar';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import VideoBox from '../components/VideoBox';
 
 function VideoPage() {
-  const [videoQuestions, setVideoQuestions] = useState([]);
-
-  useEffect(() => {
-    fetchVideoQuestions();
-  }, []);
-
-  const fetchVideoQuestions = async () => {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/video-list');
-      setVideoQuestions(response.data.videoQuestions); // Videolu soruları güncelle
-      // console.log(videoQuestions);
-    } catch (error) {
-      console.error('Error fetching video questions:', error);
-    }
-  };
-  videoQuestions.forEach((questionObj) => {
-    console.log(questionObj.media_path);
-    console.log(`WebProjectQuiz\\storage${questionObj.media_path}`);
-  }); // console.log(videoQuestions.map());
-
   return (
     <div>
       <Navbar item="video"></Navbar>
       <VideoBox></VideoBox>
-      <video width="320" height="240" controls>
-        <source
-          src="http://localhost:8000/storage/admin/questionFile/videos/1716487362_4tn12p15hk2y12.mp4"
-          type="video/mp4"
-        />
-      </video>
-      {/* <div>
-        {videoQuestions && videoQuestions.length > 0 ? (
-          videoQuestions.map((question) => (
-            <div key={question.id}>
-              <h3>
-                {question.text} - Soru Süresi {question.duration}
-              </h3>
-              {question.media_path && (
-                <video controls src={question.media_path}></video>
-              )}
-              <ul>
-                {question.answers &&
-                  question.answers.map((answer, index) => (
-                    <li key={index}>{answer.text}</li>
-                  ))}
-              </ul>
-            </div>
-          ))
-        ) : (
-          <p>Sistemde hiç video sorusu yok</p>
-        )}
-      </div> */}
     </div>
   );
 }
